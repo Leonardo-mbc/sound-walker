c004b08fa04e7d42c334c12868831c11d556cd62
LeonardoKen Orihara <leonardo.mbc17@gmail.com>
2018-11-13 19:56:28 +0900

    サンプル曲を追加

--- src/constant/music-list.ts
+++ src/constant/music-list.ts
@@ -8,8 +8,8 @@ export const musicList: MusicList = [
   [
     {
       meta: {
-        title: '24 Hours Happiness (ONE! TWO! remix)',
-        artist: 'Quickdrop feat. Toni Fox',
+        title: '24 Hours Happiness',
+        artist: 'Quickdrop',
         musicId: 'm1',
         bpm: 130.0,
         offsetTime: 0.0,
@@ -20,8 +20,8 @@ export const musicList: MusicList = [
     },
     {
       meta: {
-        title: 'music2',
-        artist: 'artist2',
+        title: '24 Hours Happiness (ONE! TWO! remix)',
+        artist: 'Quickdrop feat. Toni Fox',
         musicId: 'r1',
         bpm: 130.0,
         offsetTime: 0.0,
@@ -53,6 +53,110 @@ export const musicList: MusicList = [
         offsetTime: 0.0,
         level: 5,
         discImage: 'm2/2.png',
+      },
+      scores: [[], []],
+    },
+  ],
+  [
+    {
+      meta: {
+        title: 'Ice Cream',
+        artist: 'OMFG',
+        musicId: 'm3',
+        bpm: 130.0,
+        offsetTime: 0.0,
+        level: 5,
+        discImage: 'm3/1.png',
+      },
+      scores: [[], []],
+    },
+    {
+      meta: {
+        title: 'Hello',
+        artist: 'OMFG',
+        musicId: 'r3',
+        bpm: 130.0,
+        offsetTime: 0.0,
+        level: 5,
+        discImage: 'm3/2.png',
+      },
+      scores: [[], []],
+    },
+  ],
+  [
+    {
+      meta: {
+        title: 'Digital Desire',
+        artist: 'Gisbo & Last Of The Mohicans feat. Dionne',
+        musicId: 'm4',
+        bpm: 130.0,
+        offsetTime: 0.0,
+        level: 5,
+        discImage: 'm4/1.png',
+      },
+      scores: [[], []],
+    },
+    {
+      meta: {
+        title: 'Digital Desire (Defekt Remix, NightCore)',
+        artist: 'Gisbo & Last Of The Mohicans feat. Dionne',
+        musicId: 'r4',
+        bpm: 130.0,
+        offsetTime: 0.0,
+        level: 5,
+        discImage: 'm4/2.png',
+      },
+      scores: [[], []],
+    },
+  ],
+  [
+    {
+      meta: {
+        title: 'STARGAZER',
+        artist: 'Takahiro Eguchi',
+        musicId: 'm5',
+        bpm: 130.0,
+        offsetTime: 0.0,
+        level: 5,
+        discImage: 'm5/1.png',
+      },
+      scores: [[], []],
+    },
+    {
+      meta: {
+        title: 'Collaboration',
+        artist: 'Newton & J.Williams feat.Kjun',
+        musicId: 'r5',
+        bpm: 130.0,
+        offsetTime: 0.0,
+        level: 5,
+        discImage: 'm5/2.png',
+      },
+      scores: [[], []],
+    },
+  ],
+  [
+    {
+      meta: {
+        title: 'Where Them Girls At',
+        artist: 'David Guetta',
+        musicId: 'm6',
+        bpm: 130.0,
+        offsetTime: 0.0,
+        level: 5,
+        discImage: 'm6/1.png',
+      },
+      scores: [[], []],
+    },
+    {
+      meta: {
+        title: 'Where Them Girls At (Nightcore)',
+        artist: 'David Guetta feat.Arockalips07',
+        musicId: 'r6',
+        bpm: 130.0,
+        offsetTime: 0.0,
+        level: 5,
+        discImage: 'm6/2.png',
       },
       scores: [[], []],
     },
