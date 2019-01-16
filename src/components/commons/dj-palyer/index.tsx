13cbea3d6cbcefd03185fadbc20956f91b3bec95
LeonardoKen Orihara <leonardo.mbc17@gmail.com>
2019-01-16 22:48:58 +0900

    シェアテキストを追加

--- src/components/commons/dj-palyer/index.tsx
+++ src/components/commons/dj-palyer/index.tsx
@@ -4,7 +4,6 @@ import { MusicMetaData } from '../../pages/player/player-interfaces';
 import { FilterNode, AnalyserParams } from '../../../systems/system-interfaces';
 import { Knob } from '../knob';
 import { KaossPad } from '../kaoss-pad';
-import { BACK_TO_DJ_MODE } from '../../../constant/target-name';
 import { CircleVisualizer } from '../circle-visualizer';
 
 interface DJPlayerProps {
@@ -17,6 +16,13 @@ interface DJPlayerProps {
 }
 
 interface DJPlayerState {}
+
+const TWITTER = 'TWITTER';
+const FACEBOOK = 'FACEBOOK';
+const LINE = 'LINE';
+
+let shareUrl = '';
+let title = '';
 
 export class DJPlayer extends React.Component<DJPlayerProps, DJPlayerState> {
   private container: HTMLDivElement;
@@ -43,10 +49,40 @@ export class DJPlayer extends React.Component<DJPlayerProps, DJPlayerState> {
         (e) => {
           e.preventDefault();
 
+          switch (this.props.meta.musicId) {
+            case 'm1':
+              title = 'FastLane - NIVIRO Ft. Polly Anna';
+              break;
+            case 'r3':
+              title = 'Linked - Jim Yosef & Anna Yvette';
+              break;
+            case 'm6':
+              title = 'My Heart - Different Heaven & EH!DE';
+              break;
+            case 'm9':
+              title = 'Popsicle - LFZ';
+              break;
+          }
+          shareUrl = encodeURIComponent(
+            `https://sound-walker-dj-demo.firebaseapp.com/#/dj-player/${
+              this.props.meta.musicId
+            }`
+          );
+
           const target = (e.target as HTMLElement).getAttribute('data-target');
           switch (target) {
-            case BACK_TO_DJ_MODE:
-              this.props.backToDJMode();
+            case TWITTER:
+              const twitterText = encodeURIComponent(
+                `Playing [${title}] #SoundWalker`
+              );
+              location.href = `https://twitter.com/intent/tweet?text=${twitterText}&url=${shareUrl}`;
+              break;
+            case FACEBOOK:
+              location.href = `https://www.facebook.com/sharer/sharer.php?u=${shareUrl}`;
+              break;
+            case LINE:
+              const lineText = encodeURIComponent(`${title} from Sound Walker`);
+              location.href = `https://social-plugins.line.me/lineit/share?url=${shareUrl}&text=${lineText}`;
               break;
           }
         },
@@ -124,6 +160,23 @@ export class DJPlayer extends React.Component<DJPlayerProps, DJPlayerState> {
             />
           </div>
           <KaossPad vhSize={80} filterNode={this.props.filterNode} />
+        </div>
+        <div className={styles.systemButtons}>
+          <img
+            src="assets/images/social-icons/twitter.svg"
+            width="30px"
+            data-target={TWITTER}
+          />
+          <img
+            src="assets/images/social-icons/facebook.svg"
+            width="30px"
+            data-target={FACEBOOK}
+          />
+          <img
+            src="assets/images/social-icons/line.svg"
+            width="30px"
+            data-target={LINE}
+          />
         </div>
       </div>
     );
