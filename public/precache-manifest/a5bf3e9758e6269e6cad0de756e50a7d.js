7061d376b30b0e7357f1017fede75775754a9cf8
LeonardoKen Orihara <leonardo.mbc17@gmail.com>
2019-01-27 03:19:29 +0900

    build

---
+++ public/precache-manifest/a5bf3e9758e6269e6cad0de756e50a7d.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "90f3b3450a44761b4dc0",
+    "url": "app.js"
+  },
+  {
+    "revision": "90f3b3450a44761b4dc0",
+    "url": "style.css"
+  },
+  {
+    "revision": "e0e885f1b09a892127fdcc6422269cb2",
+    "url": "index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest/bcdf40592c94fd14225e91246c139906.js"
+  "precache-manifest/a5bf3e9758e6269e6cad0de756e50a7d.js"
 );
 
 workbox.skipWaiting();
